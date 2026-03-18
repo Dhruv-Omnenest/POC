9da9dd15ddf7cb4f0ee05e2f30eeb0e299eebf89
Dhruv-Omnenest <Dhruv.mehta@omnenest.com>
2026-03-18 15:06:29 +0530

    email deprecate for zod fix

--- src/pages/Auth/schema/forgotUserId.schema.ts
+++ src/pages/Auth/schema/forgotUserId.schema.ts
@@ -3,15 +3,17 @@ import { z } from 'zod';
 export const forgotUserIdSchema = z.object({
   email: z
     .string()
-    .min(1, 'Email is required')
     .trim()
     .toLowerCase()
-    .email('Invalid email format'),
+    .min(1, 'Email is required')
+    .pipe(z.email('Invalid email format')),
+    
   pan: z
     .string()
     .trim()
+    .toUpperCase()
     .length(10, 'PAN must be 10 characters')
-    .regex(/^[A-Z]{5}[0-9]{4}[A-Z]{1}$/i, 'Invalid PAN format'),
+    .regex(/^[A-Z]{5}[0-9]{4}[A-Z]{1}$/, 'Invalid PAN format'),
 });
 
 export type ForgotUserIdFormData = z.infer<typeof forgotUserIdSchema>;
